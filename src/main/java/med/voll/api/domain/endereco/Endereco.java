56f05c55e5a67a150b35a05e3d98766bfc628847
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:29:59 -0300

    :bricks: ci: corpo da classe endereco
    foi adicionado a estrutura padrao de endereco para ajudar na simplicidade do codigo e reutilizacao em medico e paciente

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -1,4 +1,13 @@
 package med.voll.api.domain.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
 }
