991b19b22ef624c10219e41b7aa8ba8138862a90
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:24:49 -0300

    :sparkles: feat: atributos de paciente
    foi adicionado os atributos de paciente

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,4 +1,6 @@
 package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
 import lombok.*;
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
@@ -7,5 +9,14 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+
+
+    private Boolean ativo;
 
 }
