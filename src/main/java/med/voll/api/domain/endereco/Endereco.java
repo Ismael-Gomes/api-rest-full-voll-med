1ec77cb7faebdda4588eb6e1605c1b67fd3b4185
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:31:37 -0300

    :sparkles: feat: atributos endereco
    foi adicionado os atributos de endereco

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -10,4 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
 }
