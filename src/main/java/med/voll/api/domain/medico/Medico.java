4c2a2dbadacd824d5b131238e192cbd5911e572f
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:20:07 -0300

    :bricks: ci: corpo da classe medico
    foi adicionado a estrutura padrao de medico para banco de dados e classe

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,4 +1,11 @@
 package med.voll.api.domain.medico;
+import lombok.*;
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Medico {
 
 }
