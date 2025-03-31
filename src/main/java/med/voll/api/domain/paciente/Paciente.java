7e781c5db0ac2a4ea430fcdc525d0c09d258807d
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:19:22 -0300

    :bricks: ci: corpo da classe paciente
    foi adicionado a estrutura padrao de paciente para banco de dados e classe

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,4 +1,11 @@
 package med.voll.api.domain.paciente;
+import lombok.*;
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Paciente {
 
 }
