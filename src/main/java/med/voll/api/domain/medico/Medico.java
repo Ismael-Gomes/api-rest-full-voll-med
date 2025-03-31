9c63dd6f27f6d483705c007318787b271affdb49
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-03-31 10:25:47 -0300

    :sparkles: feat: atributos de medico
    foi adicionado os atributos de medico

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,5 +1,8 @@
 package med.voll.api.domain.medico;
+
+import jakarta.persistence.*;
 import lombok.*;
+
 @Table(name = "medicos")
 @Entity(name = "Medico")
 @Getter
@@ -7,5 +10,13 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+    private Boolean ativo;
 
 }
