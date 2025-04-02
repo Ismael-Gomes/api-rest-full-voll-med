0a9938fdf377eb0c6d48f9220cca69e35094168e
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-04-02 10:48:20 -0300

    :sparkles: feat: mapeamento e JPA paciente
    foi feito o chamado o mapeamento da classe controladora de paciente, e chamado o objeto pacienteRepository onde vem o gerenciamento JPA

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,13 @@
 package med.voll.api.controller;
 
+import med.voll.api.domain.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
+
+    @Autowired
+    PacienteRepository pacienteRepository;
 
 }
