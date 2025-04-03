89d6172a34a2abe09a04198495ede861ddc3abb5
Ismael <122225294+Ismael-Gomes@users.noreply.github.com>
2025-04-03 10:09:32 -0300

    :sparkles: feat: listar medico
    foi criado a funcao de listagem de medicos onde existe uma paginacao a cada 10 medicos alem de ordenada por ordem alfabetica

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +26,11 @@ public class MedicoController {
 
         var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        var page = medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
     }
 }
